28d9ff87a8cf0766c30277036a7f88c45aa33e9e
Channu12 <channume008@gmail.com>
2022-10-10 14:45:06 +0530

    Shashi Pract

---
+++ src/test/java/NewPackage/CreateTest.java
@@ -0,0 +1,18 @@
+package NewPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+
+	@Test
+	public void pushpaTest()
+	{
+		System.out.println("Pushpa");
+	}
+	
+	@Test
+	public void rajTest()
+	{
+		System.out.println("Raj");
+	}
+}
